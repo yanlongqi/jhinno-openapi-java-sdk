89e858cc3940b6f8552bb0ee4ec448301ec05081
lqyan <lqyan@jhinno.com>
2024-07-24 23:19:23 +0800

    feat(数据): 数据增加额外的属性

--- jhinno-openapi-java-sdk/src/main/java/com/jhinno/sdk/openapi/api/file/FileInfo.java
+++ jhinno-openapi-java-sdk/src/main/java/com/jhinno/sdk/openapi/api/file/FileInfo.java
@@ -2,6 +2,8 @@ package com.jhinno.sdk.openapi.api.file;
 
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * 文件信息
  *
@@ -10,6 +12,54 @@ import lombok.Data;
  */
 @Data
 public class FileInfo {
+
+
+    /**
+     * 绝对路径
+     */
+    private String absolutePath;
+
+    /**
+     * 是否为空
+     */
+    private Boolean empty;
+
+
+    /**
+     * 文件密级
+     */
+    private Integer fileConfLevel;
+
+    /**
+     * 文件ID
+     */
+    private String fileId;
+
+    /**
+     * 是否文件夹
+     */
+    private Boolean folder;
+
+    /**
+     * ID
+     */
+    private Long id;
+
+    /**
+     * 最后修改时间
+     */
+    private Date lastModified;
+
+    /**
+     * 权限
+     */
+    private String permission;
+
+    /**
+     * 文件大小
+     */
+    private Long size;
+
     /**
      * 文件名
      */
@@ -30,9 +80,24 @@ public class FileInfo {
     private String fileType;
 
     /**
+     * 名称
+     */
+    private String name;
+
+    /**
      * 文件属主
      */
     private String owner;
+
+    /**
+     * 组
+     */
+    private String group;
+
+    /**
+     * 其他
+     */
+    private String other;
 
     /**
      * 是否可读
@@ -50,9 +115,9 @@ public class FileInfo {
     private Boolean execute;
 
     /**
-     * 是否软连接
+     * 是否软链接
      */
-    private Boolean link;
+    private Integer link;
 
 
 }
